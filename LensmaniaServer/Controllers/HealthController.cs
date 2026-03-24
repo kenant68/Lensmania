68e23f4c442a14f8b5aa5dfdf0d672e360bb66d5
TEKBAS Kenan <tekbask8@gmail.com>
2026-03-24 11:42:10 +0100

    feat(server): add health check endpoint

---
+++ LensmaniaServer/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LensmaniaServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase {
+
+    [HttpGet]
+    public IActionResult Check() {
+        return Ok(new {
+            status = "healthy",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
