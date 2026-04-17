504bc9c02e2c3b97a7bbd6944a670c56d557e114
sophie-rud <sophie.rudio@lapiscine.pro>
2026-04-17 12:35:02 +0200

    refactor(DTOs): clean DTOs and replace PostDto with explicit response models

--- LensmaniaLibrary/DTOs/Posts/CreatePostRequest.cs
+++ LensmaniaLibrary/DTOs/Posts/CreatePostRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LensmaniaLibrary.DTOs.Posts;
 
 public class CreatePostRequest
 {
-    public string? Title { get; set; } = string.Empty;
-    public required string PhotoUrl { get; set; }
-    public string? Description { get; set; } = string.Empty;
+    [MaxLength(150)]
+    public string? Title { get; set; }
+
+    [Required] 
+    public string PhotoUrl { get; set; } = string.Empty;
+    
+    [MaxLength(300)]
+    public string? Description { get; set; }
 }
