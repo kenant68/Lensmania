4d4b39a8f30d6df6dd03bef7a780701e27b17cd6
TEKBAS Kenan <tekbask8@gmail.com>
2026-04-26 18:43:16 +0200

    fix(config): fix EmailOptions

--- LensmaniaServer/Models/EmailOptions.cs
+++ LensmaniaServer/Models/EmailOptions.cs
@@ -5,7 +5,7 @@ public class EmailOptions
     public string SmtpHost { get; set; } = "";
     public int SmtpPort { get; set; }
     public bool UseStartTls { get; set; } = true;
-    public string FromAdress { get; set; } = "";
+    public string FromAddress { get; set; } = "";
     public string FromDisplayName { get; set; } = "";
     public string Username { get; set; } = "";
     public string Password { get; set; } = "";
