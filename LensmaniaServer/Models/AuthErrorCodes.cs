427c07eb3f55075fa4b78062d275450720e70ca2
TEKBAS Kenan <tekbask8@gmail.com>
2026-04-26 16:59:04 +0200

    feat(auth): add AUTH_INVALID_OR_EXPIRED_RESET_TOKEN error code

--- LensmaniaServer/Models/AuthErrorCodes.cs
+++ LensmaniaServer/Models/AuthErrorCodes.cs
@@ -5,4 +5,5 @@ public static class AuthErrorCodes
     public const string InvalidCredentials = "AUTH_INVALID_CREDENTIALS";
     public const string DuplicateIdentity = "AUTH_DUPLICATE_IDENTITY";
     public const string ValidationFailed = "AUTH_VALIDATION_FAILED";
+    public const string InvalidOrExpiredToken = "AUTH_INVALID_OR_EXPIRED_TOKEN";
 }
