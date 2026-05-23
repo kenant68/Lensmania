7066ef7dffa0378f5b9dfe1ad496d39ed85cba8b
sophie-rud <sophie.rudio@lapiscine.pro>
2026-05-23 14:16:15 +0200

    fix(db): fix isActive default value to true

--- LensmaniaServer/Migrations/20260521123126_AddIsActiveFieldToUserTable.cs
+++ LensmaniaServer/Migrations/20260521123126_AddIsActiveFieldToUserTable.cs
@@ -15,7 +15,7 @@ namespace LensmaniaServer.Migrations
                 table: "Users",
                 type: "boolean",
                 nullable: false,
-                defaultValue: false);
+                defaultValue: true);
         }
 
         /// <inheritdoc />
