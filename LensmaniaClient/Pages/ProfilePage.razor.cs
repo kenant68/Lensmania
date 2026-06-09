19c4a78bcaaaccf64119fbca09bd59bf31f6723a
TEKBAS Kenan <tekbask8@gmail.com>
2026-06-09 12:25:33 +0200

    feat(client): show earned badges on user profile

--- LensmaniaClient/Pages/ProfilePage.razor.cs
+++ LensmaniaClient/Pages/ProfilePage.razor.cs
@@ -28,11 +28,13 @@ public class ProfilePageBase : ComponentBase
     protected string? _currentUsername;
     protected string? _currentEmail;
     protected UpdateUserRequest _editModel = new();
+    protected PublicUserProfileResponse? _profile;
 
     protected override async Task OnParametersSetAsync()
     {
         _isLoading = true;
         _userNotFound = false;
+        _profile = null;
         
         var state = await AuthStateProvider.GetAuthenticationStateAsync();
         var user = state.User;
@@ -53,6 +55,8 @@ public class ProfilePageBase : ComponentBase
                 _userNotFound = true;
                 return;
             }
+
+            _profile = userSearched;
         }
         catch {
             _hasError = true;
