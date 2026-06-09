8b630adc39e45ddb6f34a715c3243dd71b1c08aa
TEKBAS Kenan <tekbask8@gmail.com>
2026-06-09 11:55:10 +0200

    feat(server): add event closure and winner fields with migration

--- LensmaniaServer/Models/Event.cs
+++ LensmaniaServer/Models/Event.cs
@@ -11,10 +11,13 @@ public class Event
     public int ThemeId { get; set; }
     public int UserId { get; set; }
     public int? CoverPhotoPostId { get; set; }
+    public DateTime? ClosedAt { get; set; }
+    public int? WinnerPostId { get; set; }
 
     public Theme Theme { get; set; } = null!;
     public User User { get; set; } = null!;
     public Post? CoverPhoto { get; set; }
+    public Post? WinnerPost { get; set; }
 
     public List<Post> Posts { get; } = new();
     public List<Badge> Badges { get; } = new();
